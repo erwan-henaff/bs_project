f7a7bb20666b568c761ba7dbda7530d118711140
erwan <erwan.in.berlin@gmx.de>
2020-02-12 16:55:56 +0100

    working on the cleaning with trophy level average

--- controllers/cleanDataController.js
+++ controllers/cleanDataController.js
@@ -8,16 +8,21 @@ const Battle = require('../models/Battles');
 
 exports.cleanData = async (req,res,next) => {
     try {
-        const battle = await Battle.find({battleTime: {$exists:false}}); 
+        const battle = await Battle.find({
+            "event.id": "15000196",
+            "event.map": "Forsaken Falls",
+            "battle.players.brawler.trophies" : { $gte: 600 },
+        }); 
     
-        battle.forEach(async el => {
-            await Battle.findOneAndDelete({_id: el._id});
-            return el._id;
-        }) 
+        
+        // battle.forEach(async el => {    
+        //     await Battle.findOneAndDelete({_id: el._id});
+        //     return el._id;
+        // }) 
 
-        const verify = await Battle.find({battleTime: {$exists:false}}); 
+        // const verify = await Battle.find({battleTime: "20200210T231727.000Z"}); 
 
-        res.status(200).send({result: verify.length});
+        res.status(200).send({result: battle.length, wholeresult: battle});
     } catch (error) {
         next(error);
     }
