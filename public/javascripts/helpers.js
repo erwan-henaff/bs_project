d8bb92e229cbae97e807e35e910691eb6114a07f
erwan <erwan.in.berlin@gmx.de>
2020-02-25 15:49:05 +0100

    commit 1 three.js, automatic path through player data in the scene implemented, path on scroll must be implemented

--- public/javascripts/helpers.js
+++ public/javascripts/helpers.js
@@ -7,80 +7,128 @@ const makePlayerContainer = (data) => {
 
     console.log(data);
 
-    playerInfoCont.innerHTML = "";
 
-    let nameDiv = document.createElement("div");
-    nameDiv.classList.add("nameDiv");
-    nameDiv.innerText = `Name : ${data.name}`;
+    let loader = new THREE.FontLoader();
 
-    let tagDiv = document.createElement("div");
-    tagDiv.classList.add("tagDiv");
-    tagDiv.innerText = `Tag : ${data.tag}`;
+    let mesh = loader.load( '../assets/font.json', function ( font ) {
+
+        let geometry = new THREE.TextGeometry( `
+${data.name}
+tag : ${data.tag}
+trophies : ${data.trophies[data.trophies.length -1].trophies}
+highest trophies : ${data.highestTrophies[data.highestTrophies.length -1].highestTrophies}
+Experience Level : ${data.expLevel}
+3vs3 Victories : ${data["3vs3Victories"][data["3vs3Victories"].length -1]["3vs3Victories"]}
+solo Victories : ${data.soloVictories[data.soloVictories.length -1].soloVictories}
+duo Victories : ${data.duoVictories[data.duoVictories.length -1].duoVictories}
+Power Play Points : ${data.powerPlayPoints? data.powerPlayPoints : 0}
+Highest Power Play Points : ${data.highestPowerPlayPoints? data.highestPowerPlayPoints : 0}
+Best time at Robo Rumble : ${(data.bestRoboRumbleTime - data.bestRoboRumbleTime%60)/60}m${data.bestRoboRumbleTime%60}s
+Best time as Big Brawler : ${(data.bestTimeAsBigBrawler-data.bestTimeAsBigBrawler%60)/60}m${data.bestTimeAsBigBrawler%60}s
 
 
-    let trophiesDiv = document.createElement("div");
-    trophiesDiv.classList.add("trophiesDiv");
-    trophiesDiv.innerText = `Trophies : ${data.trophies[data.trophies.length -1].trophies}`;
+        `, {
+            font: font,
+            size: 1,
+            height: 0.2,
+            curveSegments: 2,
+            bevelEnabled: false,
+            bevelThickness: 10,
+            bevelSize: 8,
+            bevelOffset: 0,
+            bevelSegments: 5
+        } );
+        let material = new THREE.MeshNormalMaterial();
+            
+        let mesh2 = new THREE.Mesh(geometry, material);
+        mesh2.position.x = -10;
+        mesh2.position.y = 10;
+        mesh2.position.z = 20;
+        scene.add(mesh2);
+    } );
 
-    let highestTrophiesDiv = document.createElement("div");
-    highestTrophiesDiv.classList.add("highestTrophiesDiv");
-    highestTrophiesDiv.innerText = `Highest trophies : ${data.highestTrophies[data.highestTrophies.length -1].highestTrophies}`;
-
-    let expLevelDiv = document.createElement("div");
-    expLevelDiv.classList.add("expLevelDiv");
-    expLevelDiv.innerText = `Experience Level : ${data.expLevel}`;
-
-    let _3vs3Div = document.createElement("div");
-    _3vs3Div.classList.add("_3vs3Div");
-    _3vs3Div.innerText = `3vs3 Victories : ${data["3vs3Victories"][data["3vs3Victories"].length -1]["3vs3Victories"]}`;
-
-    let soloDiv = document.createElement("div");
-    soloDiv.classList.add("soloDiv");
-    soloDiv.innerText = `solo Victories : ${data.soloVictories[data.soloVictories.length -1].soloVictories}`;
-
-    let duoDiv = document.createElement("div");
-    duoDiv.classList.add("duoDiv");
-    duoDiv.innerText = `duo Victories : ${data.duoVictories[data.duoVictories.length -1].duoVictories}`;
-
-    let powerPlayDiv = document.createElement("div");
-    powerPlayDiv.classList.add("powerPlayDiv");
-    powerPlayDiv.innerText = `Power Play Points : ${data.powerPlayPoints}`;
-
-    let powerPlayHighDiv = document.createElement("div");
-    powerPlayHighDiv.classList.add("powerPlayHighDiv");
-    powerPlayHighDiv.innerText = `Highest Power Play Points : ${data.highestPowerPlayPoints}`;
-
-    let bestRoboRumbleTimeDiv = document.createElement("div");
-    bestRoboRumbleTimeDiv.classList.add("bestRoboRumbleTimeDiv");
-    bestRoboRumbleTimeDiv.innerText = `Best time at Robo Rumble : ${(data.bestRoboRumbleTime - data.bestRoboRumbleTime%60)/60}m${data.bestRoboRumbleTime%60}s`;
-    
-    let bestTimeAsBigBrawlerDiv = document.createElement("div");
-    bestTimeAsBigBrawlerDiv.classList.add("bestTimeAsBigBrawlerDiv");
-    bestTimeAsBigBrawlerDiv.innerText = `Best time as Big Brawler : ${(data.bestTimeAsBigBrawler-data.bestTimeAsBigBrawler%60)/60}m${data.bestTimeAsBigBrawler%60}s`;
-
-    let brawlersDiv = document.createElement("div");
-    brawlersDiv.classList.add("brawlersDiv");
-    brawlersDiv.innerHTML = "<h3>Brawlers</h1>"
+    ///// handles the text for each brawlers 
 
     for (let i = 0; i < data.brawlers.length; i++) {
-        let BrawlerDiv = makeBrawlerCont(data.brawlers[i]);
-        brawlersDiv.appendChild(BrawlerDiv);
+        makeBrawlerText(data.brawlers[i], i);
+        
     }
 
 
-    playerInfoCont.appendChild(nameDiv);
-    playerInfoCont.appendChild(tagDiv);
-    playerInfoCont.appendChild(trophiesDiv);
-    playerInfoCont.appendChild(highestTrophiesDiv);
-    playerInfoCont.appendChild(_3vs3Div);
-    playerInfoCont.appendChild(expLevelDiv);
-    playerInfoCont.appendChild(soloDiv);
-    playerInfoCont.appendChild(powerPlayDiv);
-    playerInfoCont.appendChild(duoDiv);
-    playerInfoCont.appendChild(powerPlayHighDiv);
-    playerInfoCont.appendChild(bestRoboRumbleTimeDiv);
-    playerInfoCont.appendChild(bestTimeAsBigBrawlerDiv);
-    playerInfoCont.appendChild(brawlersDiv);
+    // playerInfoCont.innerHTML = "";
+
+    // let nameDiv = document.createElement("div");
+    // nameDiv.classList.add("nameDiv");
+    // nameDiv.innerText = `Name : ${data.name}`;
+
+    // let tagDiv = document.createElement("div");
+    // tagDiv.classList.add("tagDiv");
+    // tagDiv.innerText = `Tag : ${data.tag}`;
+
+
+    // let trophiesDiv = document.createElement("div");
+    // trophiesDiv.classList.add("trophiesDiv");
+    // trophiesDiv.innerText = `Trophies : ${data.trophies[data.trophies.length -1].trophies}`;
+
+    // let highestTrophiesDiv = document.createElement("div");
+    // highestTrophiesDiv.classList.add("highestTrophiesDiv");
+    // highestTrophiesDiv.innerText = `Highest trophies : ${data.highestTrophies[data.highestTrophies.length -1].highestTrophies}`;
+
+    // let expLevelDiv = document.createElement("div");
+    // expLevelDiv.classList.add("expLevelDiv");
+    // expLevelDiv.innerText = `Experience Level : ${data.expLevel}`;
+
+    // let _3vs3Div = document.createElement("div");
+    // _3vs3Div.classList.add("_3vs3Div");
+    // _3vs3Div.innerText = `3vs3 Victories : ${data["3vs3Victories"][data["3vs3Victories"].length -1]["3vs3Victories"]}`;
+
+    // let soloDiv = document.createElement("div");
+    // soloDiv.classList.add("soloDiv");
+    // soloDiv.innerText = `solo Victories : ${data.soloVictories[data.soloVictories.length -1].soloVictories}`;
+
+    // let duoDiv = document.createElement("div");
+    // duoDiv.classList.add("duoDiv");
+    // duoDiv.innerText = `duo Victories : ${data.duoVictories[data.duoVictories.length -1].duoVictories}`;
+
+    // let powerPlayDiv = document.createElement("div");
+    // powerPlayDiv.classList.add("powerPlayDiv");
+    // powerPlayDiv.innerText = `Power Play Points : ${data.powerPlayPoints}`;
+
+    // let powerPlayHighDiv = document.createElement("div");
+    // powerPlayHighDiv.classList.add("powerPlayHighDiv");
+    // powerPlayHighDiv.innerText = `Highest Power Play Points : ${data.highestPowerPlayPoints}`;
+
+    // let bestRoboRumbleTimeDiv = document.createElement("div");
+    // bestRoboRumbleTimeDiv.classList.add("bestRoboRumbleTimeDiv");
+    // bestRoboRumbleTimeDiv.innerText = `Best time at Robo Rumble : ${(data.bestRoboRumbleTime - data.bestRoboRumbleTime%60)/60}m${data.bestRoboRumbleTime%60}s`;
+    
+    // let bestTimeAsBigBrawlerDiv = document.createElement("div");
+    // bestTimeAsBigBrawlerDiv.classList.add("bestTimeAsBigBrawlerDiv");
+    // bestTimeAsBigBrawlerDiv.innerText = `Best time as Big Brawler : ${(data.bestTimeAsBigBrawler-data.bestTimeAsBigBrawler%60)/60}m${data.bestTimeAsBigBrawler%60}s`;
+
+    // let brawlersDiv = document.createElement("div");
+    // brawlersDiv.classList.add("brawlersDiv");
+    // brawlersDiv.innerHTML = "<h3>Brawlers</h1>"
+
+    // for (let i = 0; i < data.brawlers.length; i++) {
+    //     let BrawlerDiv = makeBrawlerCont(data.brawlers[i]);
+    //     brawlersDiv.appendChild(BrawlerDiv);
+    // }
+
+
+    // playerInfoCont.appendChild(nameDiv);
+    // playerInfoCont.appendChild(tagDiv);
+    // playerInfoCont.appendChild(trophiesDiv);
+    // playerInfoCont.appendChild(highestTrophiesDiv);
+    // playerInfoCont.appendChild(_3vs3Div);
+    // playerInfoCont.appendChild(expLevelDiv);
+    // playerInfoCont.appendChild(soloDiv);
+    // playerInfoCont.appendChild(powerPlayDiv);
+    // playerInfoCont.appendChild(duoDiv);
+    // playerInfoCont.appendChild(powerPlayHighDiv);
+    // playerInfoCont.appendChild(bestRoboRumbleTimeDiv);
+    // playerInfoCont.appendChild(bestTimeAsBigBrawlerDiv);
+    // playerInfoCont.appendChild(brawlersDiv);
 
 } 
 
@@ -93,4 +141,38 @@ const makeBrawlerCont = (data) => {
     brawlerDiv.innerHTML = `${data.name}<br>Power : ${data.power}<br>Rank : ${data.rank}<br>Trophies : ${data.trophies[data.trophies.length -1].trophies} <br>Highest Trophies : ${data.highestTrophies}<img src ="${urlPicture}" alt="picture" width="100%">`;
 
     return brawlerDiv;
+}
+
+const makeBrawlerText = (data, distance) => {
+
+    let loader = new THREE.FontLoader();
+
+    let mesh = loader.load( '../assets/font.json', function ( font ) {
+
+        let geometry = new THREE.TextGeometry( `
+${data.name}
+Power : ${data.power}
+Rank : ${data.rank}
+Trophies : ${data.trophies[data.trophies.length -1].trophies}
+Highest Trophies : ${data.highestTrophies}
+`           , {
+            font: font,
+            size: 1,
+            height: 0.2,
+            curveSegments: 2,
+            bevelEnabled: false,
+            bevelThickness: 10,
+            bevelSize: 8,
+            bevelOffset: 0,
+            bevelSegments: 5
+        } );
+        let material = new THREE.MeshNormalMaterial();
+            
+        let mesh2 = new THREE.Mesh(geometry, material);
+        mesh2.position.x = -10;
+        mesh2.position.y = 10 - 20 * (distance%2);
+        mesh2.position.z = 20 - distance * 30;
+        scene.add(mesh2);
+    } );
+
 }
--- public/javascripts/indexFrontEnd.js
+++ public/javascripts/indexFrontEnd.js
@@ -1,10 +1,85 @@
-// import { makePlayerContainer }  from './helpers'
-
-
-
 let tag_input = document.getElementById("tag_input");
-
 let submit_button = document.getElementById("submit_button")
+
+/////// setting up the three.js scene into mainCont
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1200 );
+camera.position.z = 60;
+camera.lookAt(0,0,-1200);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+let cont = document.getElementById('mainCont');
+cont.appendChild( renderer.domElement );
+
+
+var geometry = new THREE.IcosahedronGeometry(6, 2);
+// var material = new THREE.MeshLambertMaterial( { color: 0x5029ff } );
+var edges = new THREE.EdgesGeometry( geometry );
+var line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
+scene.add( line );
+
+
+var loader = new THREE.FontLoader();
+
+
+let mesh3 = loader.load( '../assets/font.json', function ( font ) {
+
+	let geometry = new THREE.TextGeometry( 'BRAWL DATA', {
+		font: font,
+		size: 6,
+		height: 3,
+        curveSegments: 2,
+		bevelEnabled: false,
+		bevelThickness: 10,
+		bevelSize: 8,
+		bevelOffset: 0,
+		bevelSegments: 5
+    } );
+    let material = new THREE.MeshNormalMaterial();
+        
+    let mesh2 = new THREE.Mesh(geometry, material);
+    mesh2.position.x = -30;
+    mesh2.position.y = -60;
+    mesh2.position.z = -100;
+    scene.add(mesh2);
+} );
+
+var sampleClosedSpline = new THREE.CatmullRomCurve3( [
+    new THREE.Vector3( 0, 0, 60 ),
+    new THREE.Vector3( 0, 0, -900)
+], true );
+
+sampleClosedSpline.curveType = "catmullrom";
+sampleClosedSpline.tension = 0.6;
+
+var camPosIndex = 0;
+
+function updateCamera(ev) {
+    // ev.preventDefault();
+    camera.position.z = 60 - window.scrollY / 5;
+    console.log("scrollin ...")
+}
+renderer.domElement.addEventListener("scroll", updateCamera);
+
+
+function animate() {
+    requestAnimationFrame( animate );
+    // camPosIndex++;
+    // if (camPosIndex > 5000) {
+    //     camPosIndex = 0;
+    // }
+    // var camPos = sampleClosedSpline.getPoint(camPosIndex / 5000);
+    // camera.position.x = camPos.x;
+    // camera.position.y = camPos.y;
+    // camera.position.z = camPos.z;
+
+    renderer.render( scene, camera );
+    line.rotation.y += 0.005;
+}
+animate();
+
+
 
 
 
